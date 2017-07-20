12639f9eb902bfcee6840195c59cd5a34f534c24
Sayali Sonawane <sayasonawane11@gmail.com>
2017-07-20 12:41:50 +0530

    Add files via upload

---
+++ Bubble_Chart/bubble_chart.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 20 12:00:27 2017
+
+@author: Sayali Sonawane
+"""
+# Import packages
+import pandas as pd
+import plotly
+from plotly.graph_objs import Scatter,Layout,Marker
+
+# Read data
+# Dataset: Life Expectancy and per capita income (Rosling) 
+df = pd.read_csv('/sayali/Desktop/Data Visualisation/Scatterplot/Multi_trace_scatterplot/rosling_full.csv')
+
+# Define trace
+# Create multiple  traces for each continent
+# Bubble chart is extended version of Scatterplot. Only add Marker(size) in scatterplot to convert it into bubble chart. 
+trace1 = Scatter(x= df['"Africa, x"'], y=df['"Africa, y"'],marker=Marker(size= df['"Africa, size"']),mode = 'markers',showlegend=True,name='Africa')
+trace2 = Scatter(x= df['"Americas, x"'], y=df['"Americas, y"'],marker=Marker(size= df['"Americas, size"']),mode = 'markers',showlegend=True,name='America')
+trace3 = Scatter(x= df['"Asia, x"'], y=df['"Asia, y"'],marker=Marker(size= df['"Asia, size"']),mode = 'markers',showlegend=True,name='Asia')
+trace4 = Scatter(x= df['"Europe, x"'], y=df['"Europe, y"'],marker=Marker(size= df['"Europe, size"']), mode = 'markers',showlegend=True,name='Europe')
+trace5 = Scatter(x= df['"Oceania, x"'], y=df['"Oceania, y"'],marker=Marker(size= df['"Oceania, size"']), mode = 'markers',showlegend=True,name='Oceania')
+
+# Create chart 
+# Output will be stored as a html file. 
+# Whenever we will open output html file, one popup option will ask us about if want to save it in jpeg format. 
+# If you want basic bubble chart with only one continent just include that particular trace while providing input. 
+# Example of basic bubble chart
+# "data":[trace1]
+plotly.offline.plot({
+                         "data": [trace1,trace2,trace3,trace4,trace5], 
+                         # <b> and </b>: Bold title 
+                         # <br> use this for line break.
+                        "layout": Layout(title="<b> GDP Per Capita Vs Life Expectancy</b>", 
+                                                   xaxis= dict(
+                                                        title= '<b>GDP Per Capita</b>',
+                                                        zeroline= False,
+                                                        gridcolor='rgb(183,183,183)',
+                                                        showline=True
+                                                    ),
+                                                    yaxis=dict(
+                                                        title= '<b>Life Expectancy</b>',
+                                                        gridcolor='rgb(183,183,183)',
+                                                        zeroline=False,
+                                                        showline=True
+                                                    ))
+                                  },filename='bubble_chart.html',image='jpeg')  
